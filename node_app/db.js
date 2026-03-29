b0b041d710650a1430910b74f2996f144bf99229
jaaywill4 <jw982052@wcupa.edu>
2026-03-29 19:11:10 -0400

    Initialize MongoDB connection in db.js
    Set up MongoDB connection with error handling and logging.

--- node_app/db.js
+++ node_app/db.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
 
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "sammy";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "sharkpass";
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || "mongo";
+const MONGO_PORT = process.env.MONGO_PORT || "27017";
+const MONGO_DB = process.env.MONGO_DB || "sharkinfo";
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(url);
+
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+module.exports = db;
