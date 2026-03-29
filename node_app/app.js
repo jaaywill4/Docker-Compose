0b2c642e48c6c8ed49b5f22c17be7edd90787621
jaaywill4 <jw982052@wcupa.edu>
2026-03-29 19:12:04 -0400

    Set up Express server with routes for sharks

--- node_app/app.js
+++ node_app/app.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const path = require("path");
+const db = require("./db");
+const sharkController = require("./controllers/sharks");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/sharks", sharkController.index);
+app.post("/sharks/addshark", sharkController.addShark);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
